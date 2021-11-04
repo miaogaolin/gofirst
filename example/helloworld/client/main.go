87d0fef220a03d96a970c3f9990ed061583b69c5
DESKTOP-RKMKLHA\owner <2825873215@QQ.COM>
2021-11-04 11:32:39 +0800

    update example

--- example/helloworld/client/main.go
+++ example/helloworld/client/main.go
@@ -6,8 +6,8 @@ import (
 	"os"
 	"time"
 
+	pb "github.com/miaogaolin/gofirst/example/helloworld"
 	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
 const (
@@ -16,24 +16,33 @@ const (
 )
 
 func main() {
-	// Set up a connection to the server.
+	// 连接服务端
+	// grpc.WithInsecure() 跳过安全验证，即明文传输
+	// grpc.WithBlock() 等待与服务端握手成功
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
+
+	// 客户端实例化
 	c := pb.NewGreeterClient(conn)
 
-	// Contact the server and print out its response.
+	// 参数
 	name := defaultName
 	if len(os.Args) > 1 {
 		name = os.Args[1]
 	}
+
+	// 连接超时设置
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
+
+	// 远程调用
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
+	// 返回消息
 	log.Printf("Greeting: %s", r.GetMessage())
 }
--- example/helloworld/server/main.go
+++ example/helloworld/server/main.go
@@ -5,33 +5,43 @@ import (
 	"log"
 	"net"
 
+	pb "github.com/miaogaolin/gofirst/example/helloworld"
+
 	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
 const (
 	port = ":50051"
 )
 
-// server is used to implement helloworld.GreeterServer.
+// 实现服务端 GreeterServer 接口
 type server struct {
+	// 必须嵌套
+	// 包含了 GreeterServer 接口其它方法实现
 	pb.UnimplementedGreeterServer
 }
 
-// SayHello implements helloworld.GreeterServer
+// 远程调用方法的具体逻辑
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
 }
 
 func main() {
+	// 监听端口，用于接受客户端的请求
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
+	// 服务端实例化
 	s := grpc.NewServer()
+
+	// 将具体的实现注册到服务端
 	pb.RegisterGreeterServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
+
+	// 阻塞等待
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
